2ab266ac33d9477ecf8c65a7c9408c66647e1b9d
deniseelima <deniiselimaa21@gmail.com>
2023-07-05 10:05:38 -0300

    conversor-moedas

---
+++ projeto-01.js
@@ -0,0 +1,7 @@
+const convertButton = document.getElementById ("convert-button")
+
+function convertValues() {
+  console.log ("funcionou?")
+}
+
+convertButton.addEventListener("click", convertValues)
